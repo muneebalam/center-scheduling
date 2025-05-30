96a5e01db5ddc182bcd3caa71c818ba3ac9d438d
Muneeb Alam <muneeb.alam@gmail.com>
2025-05-30 12:06:08 -0400

    delete old app

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Center Scheduling")
-
-st.write("This is a web app to schedule staff for a center.")
-
-st.write("Upload the center data to get started.")
-
-st.file_uploader("Upload the center data", type="xlsx")
