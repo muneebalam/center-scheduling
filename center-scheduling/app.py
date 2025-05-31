d418a80a419bc73571781b18621a1378e484d7ee
Muneeb Alam <muneeb.alam@gmail.com>
2025-05-31 11:14:44 -0400

    bug fix

--- center-scheduling/app.py
+++ center-scheduling/app.py
@@ -16,8 +16,7 @@ def _get_original_dir():
 ORIGINAL_WD = _get_original_dir()
 NEEDED_WD = os.path.join(ORIGINAL_WD, BASE_FOLDER) if BASE_FOLDER in os.listdir(ORIGINAL_WD) else ORIGINAL_WD
 def _get_catalog(env):
-    os.chdir(NEEDED_WD)
-    cat_path = os.path.join("conf", env, "catalog.yml")
+    cat_path = os.path.join(NEEDED_WD,"conf", env, "catalog.yml")
     with open(cat_path, "r") as f:
         return yaml.safe_load(f)
 @st.cache_data
@@ -29,16 +28,14 @@ def _get_local_catalog():
 @st.cache_data
 def _get_example_data(catalog):
     example_data = {}
-    os.chdir(NEEDED_WD)
     for key in catalog:
-        fpath = os.path.join(catalog[key]["filepath"])
+        fpath = os.path.join(NEEDED_WD, catalog[key]["filepath"])
         if "01_raw" in fpath:
             sheet_name = catalog[key]["load_args"]["sheet_name"]
             example_data[sheet_name] = pd.read_excel(fpath, 
                                                     sheet_name=sheet_name)
     return example_data
 
-os.chdir(NEEDED_WD)
 ORIGINAL_CATALOG = _get_original_catalog()
 LOCAL_CATALOG = _get_local_catalog()
 example_data = _get_example_data(ORIGINAL_CATALOG)
@@ -65,13 +62,13 @@ with st.container(border=True):
             multiframe[sheet_name] = pd.read_excel(uploaded_file, sheet_name=sheet_name)
 
         # Then, save the data to the local (not base) catalog
-        fpath = LOCAL_CATALOG["center_hours"]["filepath"]
+        fpath = os.path.join(NEEDED_WD, LOCAL_CATALOG["center_hours"]["filepath"])
         dataset = ExcelDataset(filepath=fpath, load_args = {"sheet_name": None})
         dataset.save(multiframe)
         st.write("Upload successful")
 
         for key in LOCAL_CATALOG:
-            fpath = LOCAL_CATALOG[key]["filepath"]
+            fpath = os.path.join(NEEDED_WD, LOCAL_CATALOG[key]["filepath"])
             if "01_raw" in fpath:
                 sheet_name = LOCAL_CATALOG[key]["load_args"]["sheet_name"]
                 new_data[sheet_name] = pd.read_excel(fpath, 
@@ -88,8 +85,9 @@ with st.container(border=True):
     env_to_run = {"example": "base", "uploaded": "local"}[env_selection]
     if st.button("Run pipeline"):
         with st.spinner("Running pipeline..."):
-            os.chdir(NEEDED_WD)
             command = ["uv", "run", "kedro", "run", f"--env={env_to_run}"]
+            if NEEDED_WD != ORIGINAL_WD:
+                command = ["cd", BASE_FOLDER] + command
             process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
 
 
@@ -113,7 +111,7 @@ with st.container(border=True):
 
     res_tabs = st.tabs(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Log"])
     try:
-        results = [pd.read_csv(f"data/08_reporting/d{i}_solution.csv") for i in range(1, 6)]
+        results = [pd.read_csv(os.path.join(NEEDED_WD, "data", "08_reporting", f"d{i}_solution.csv")) for i in range(1, 6)]
         res_styled = [result.drop("Day", axis=1)
                         .set_index("Time Block")
                         .style.applymap(_apply_bg_color) for result in results]
